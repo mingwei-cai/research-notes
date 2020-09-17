5430c080d41b63601e2e8fa369b34fdfeec2cb9b
蔡明崴 <weifongkimo@gmail.com>
2020-09-17 14:54:54 +0800

    3D運算測試

--- polyhedra/js/Platonic-solid.js
+++ polyhedra/js/Platonic-solid.js
@@ -3,8 +3,12 @@ let vLight = new Vector3D(0, 3, 4);
 let focal = 12;
 let size = 480;
 
+let SinDeg = (deg) => (Math.sin(deg * Math.PI / 180));
+let CosDeg = (deg) => (Math.cos(deg * Math.PI / 180));
+
 !function () {
 	let oV06 = new Vector3D(1, 1, 1);
+	oV06 = oV06.Div(oV06.Length);
 	let oF06 = new Group([
 		new Transform(oV06, (v) => (new Vector3D(+v.x, +v.y, +v.z))),
 		new Transform(oV06, (v) => (new Vector3D(+v.x, -v.y, +v.z))),
@@ -21,5 +25,15 @@ let size = 480;
 	]);
 	let s06 = new Solid(oS06, new Color(0xCC, 0x99, 0xFF, 0.8));
 	let painter06 = new Painter(document.querySelector('canvas.p04'), vLight, focal, size);
-	painter06.Draw(s06);
+	let startTime = Date.now();
+	let DrawFrame = function () {
+		let deg = (Date.now() - startTime) * 45 / 1000;
+		painter06.Draw(new Transform(s06, (v) => (new Vector3D(
+			v.x * CosDeg(deg) + v.z * SinDeg(deg),
+			v.y * CosDeg(15) - (v.z * CosDeg(deg) - v.x * SinDeg(deg)) * SinDeg(15),
+			(v.z * CosDeg(deg) - v.x * SinDeg(deg)) * CosDeg(15) + v.y * SinDeg(15),
+		))));
+		requestAnimationFrame(DrawFrame);
+	};
+	requestAnimationFrame(DrawFrame);
 }();
